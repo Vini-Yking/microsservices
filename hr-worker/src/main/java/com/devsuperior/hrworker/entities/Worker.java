bed59c4225825738e496f19fbe8473df0a5f86df
Vini-Yking <mvdsfg@gmail.com>
2022-08-08 22:23:38 -0300

    hr worker

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -0,0 +1,45 @@
+package com.devsuperior.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@Column(name = "daily_Income")
+	private Double dailyIncome;
+
+	public Worker(Long id, String name, Double dailyIncome) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+
+}
