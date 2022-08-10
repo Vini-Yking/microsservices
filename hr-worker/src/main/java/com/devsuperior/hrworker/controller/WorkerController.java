1727451f32129e5ac7a151fa8962ad70946fd3e7
Vini-Yking <mvdsfg@gmail.com>
2022-08-10 19:53:29 -0300

    LoadBalacing

--- hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package com.devsuperior.hrworker.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +19,14 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+	
 	@Autowired
 	private WorkerRepository workerRepository;
-
+	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = workerRepository.findAll();
@@ -27,6 +35,9 @@ public class WorkerController {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity <Worker> findById(@PathVariable Long id) {
+		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
